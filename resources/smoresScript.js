da0289c851f47009db39820a7592e02dbae6a270
Niflheim-uk <loom-narwhal-0i@icloud.com>
2023-09-10 19:10:19 +0100

    bug

--- resources/smoresScript.js
+++ resources/smoresScript.js
@@ -24,7 +24,7 @@ function initialize() {
     }
   }
   el = document.getElementById('NewTrace');
-  if(el !== undefined) {
+  if(el !== null) {
     el.addEventListener('click', traceAddOnClick);
   }
   elements = document.getElementsByClassName('editSubmit');
